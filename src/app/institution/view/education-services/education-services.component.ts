1a02a3e2de43f202adaff5646e3c5d0a43ae1a6c
UnEpicFail <unepicfail@gmail.com>
2017-07-06 16:28:07 +0300

    education-services

--- src/app/institution/view/education-services/education-services.component.ts
+++ src/app/institution/view/education-services/education-services.component.ts
@@ -12,8 +12,11 @@ import { EducationServiceList } from '../../../api/institution/EducationServiceL
 })
 export class EducationServicesComponent implements OnInit {
 
-  private _institution: Institution
-  public _educationServicesList: EducationServiceList;
+  _institution: Institution
+  _params: {
+    tabName: string
+  }
+  educationServices;
 
 
   @Input()
@@ -21,9 +24,15 @@ export class EducationServicesComponent implements OnInit {
     if (institution) {
       this._institution = new Institution(institution);
       this.is.educationServiceList(null,null,null,[this._institution.identity.id]).subscribe(res => {
-        let _res = new Response(res)
-        this._educationServicesList = new EducationServiceList(_res.data)
+        this.educationServices = new EducationServiceList(new Response(res).data).items
       })
+    }
+  }
+
+  @Input()
+  set params(params) {
+    this._params = {
+      tabName: params[0]
     }
   }
 
